651024dfa9146c28db8ce9f85d65f21d5267249b
nitindhiman832 <nitindhiman832@gmail.com>
2021-03-22 04:19:32 -0700

    a

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT||9800;
+
+app.use(express.static(path.join(__dirname,'build')));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(_dirname,'build/index.html'))
+});
+
+app.listen(port);
